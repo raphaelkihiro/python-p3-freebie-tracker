023518388d3d5fa1310cd17e75888c458712b69f
raphael kihiro <kihirongigi@gmail.com>
2025-06-03 19:49:40 +0300

    Created a freebies table

--- lib/models.py
+++ lib/models.py
@@ -27,3 +27,19 @@ class Dev(Base):
 
     def __repr__(self):
         return f'<Dev {self.name}>'
+    
+class Freebie(Base):
+    __tablename__ = 'freebies'
+
+    id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    value = Column(Integer)
+
+    dev_id = Column(Integer, ForeignKey('devs.id'))
+    company_id = Column(Integer, ForeignKey('companies.id'))
+
+    dev = relationship("Dev", back_populates="freebies")
+    company = relationship("Company", back_populates="freebies")
+    
+    def print_details(self):
+        return f"{self.dev.name} owns a {self.item_name} from {self.company.name}"
